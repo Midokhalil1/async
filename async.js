965a55492594971a9734608f15f5ccfdff2d6b52
Midokhalil1 <midokhalil22@gmail.com>
2022-10-11 11:44:20 -0400

    .

---
+++ async.js
@@ -0,0 +1,24 @@
+import fs from "fs"
+const filename = "file.txt"
+
+// call back 
+// fs.readFile(filename,"utf8",function (err,data) {
+//     console.log(data);
+// })
+ 
+
+// promise (then)
+// fs.readFile(filename,"utf8")
+// .then((data) => {
+//     console.log(data);
+// })
+
+
+// async 
+const showText = async () => {
+// async function showText() {
+const data = await fs.readFileSync(filename,"utf8");
+return data 
+}
+console.log (await showText())
+
